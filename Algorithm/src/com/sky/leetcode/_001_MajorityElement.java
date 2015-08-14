7eb046de86afa9165a8ef9284303878e0141342e
SkyDream <SkyDream@DuanSky>
2015-08-14 22:17:39 +0800

    算法01天

---
+++ Algorithm/src/com/sky/leetcode/_001_MajorityElement.java
@@ -0,0 +1,33 @@
+package com.sky.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _001_MajorityElement {
+	
+	public static void main(String args[]){
+		int[] nums={1,1,2,2,2,3,3,3,3,1,1,1};
+		System.out.println(majorityElement(nums));
+	}
+	
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        int maxCounter=1;
+        int majorityElement=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int counter=map.get(nums[i]);
+                if(counter+1>maxCounter){
+                	maxCounter=counter+1;
+                	majorityElement=nums[i];
+                }
+                map.put(nums[i],counter+1);
+            }
+            else
+                map.put(nums[i],1);
+        }
+        return majorityElement;
+    }
+}
+
