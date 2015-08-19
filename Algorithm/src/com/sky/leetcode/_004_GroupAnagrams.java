fa13869737f9658a684002829b4d71cafb6be9c5
SkyDream <SkyDream@DuanSky>
2015-08-19 22:25:48 +0800

    完成04算法

--- Algorithm/src/com/sky/leetcode/_004_GroupAnagrams.java
+++ Algorithm/src/com/sky/leetcode/_004_GroupAnagrams.java
@@ -1,21 +1,65 @@
 package com.sky.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /*
  * 这道题的难点在于如何对于特殊情况的处理：
  * 如下：[""],["",""],["a"]
+ * 学习Arrays.asList();
  */
 public class _004_GroupAnagrams {
 	
 	public static void main(String args[]){
-		String[] strs={"a"};
+		String[] strs={"and","dna"};
+//		List<String> list=Arrays.asList(new String());
+//		list.add(new String());
 		groupAnagrams(strs);
 	}
-
+	public static List<List<String>> groupAnagrams(String[] strs){
+		List<List<String>> list=new ArrayList<List<String>>();
+		Map<String,List<String>> map=new HashMap<String,List<String>>();
+		if(strs.length==0)
+			return list;
+		int n=strs.length;
+		for(int i=0;i<n;i++){
+			String temp=strs[i];
+			char[] tempChar=temp.toCharArray();
+			Arrays.sort(tempChar);
+			temp=new String(tempChar);
+			if(temp.isEmpty()){
+				if(map.containsKey("_empty_"))
+					map.get("_empty_").add(new String());
+				else{
+					List<String> tempList=new ArrayList<String>();
+					tempList.add(new String());
+					map.put("_empty_", tempList);
+				}
+			}
+			else{
+				if(map.containsKey(temp))
+					map.get(temp).add(strs[i]);
+				else{
+					List<String> tempList=new ArrayList<String>();
+					tempList.add(strs[i]);
+					map.put(temp, tempList);
+				}
+			}
+			
+		}
+		for(Entry<String,List<String>> entry : map.entrySet()){
+			List<String> tempList=entry.getValue();
+			Collections.sort(tempList);
+			list.add(tempList);
+		}
+		return list;
+	}
+/*
 	public static List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> list=new ArrayList<List<String>>();
         //保存类型与数值的映射
@@ -60,4 +104,5 @@ public class _004_GroupAnagrams {
         }
         return list;
     }
+    */
 }
