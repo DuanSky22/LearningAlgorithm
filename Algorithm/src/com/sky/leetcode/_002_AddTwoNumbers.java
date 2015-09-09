0a48ef93c4f4e5290587ac0563c69c802c71cf25
SkyDream <SkyDream@DuanSky>
2015-09-09 20:47:24 +0800

    modify njz list node

---
+++ Algorithm/src/com/sky/leetcode/_002_AddTwoNumbers.java
@@ -0,0 +1,24 @@
+/**
+* @author DuanSky
+* @date 2015年9月7日 下午11:02:41
+* @content 
+*/
+package com.sky.leetcode;
+
+import com.njz.leetcode.ListNode;
+
+  //Definition for singly-linked list.
+/*Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+*/
+
+public class _002_AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {      
+        if(l1==null || l2==null)
+        	return l1==null ? l2 : l1;
+        ListNode result=null;
+        
+        return result;
+        
+    }
+}
